10615113dc6a1d23b1b04c0fdde0c89c6470a628
Arath Morales <arath020310@gmail.com>
2023-03-17 02:39:23 -0600

    Funcionalidad de personaje

--- JS/app.js
+++ JS/app.js
@@ -5,6 +5,7 @@ const apiRick = async (pagina) => {
     const data = await api.json();
     console.log(data)
     divRes= document.querySelector('#resultado');
+    divRes.innerHTML = ''
 
 
     data.results.map(item => {
@@ -13,9 +14,17 @@ const apiRick = async (pagina) => {
         <div class="card" style="width: 18rem;">
         <img src="${item.image}" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title"> ${item.name}</h5>
+            <p class="card-text">
+                <b>Estatus: </b>${item.status}
+            </p>
+            <p class="card-text">
+                <b>Especie: </b>${item.species}
+            </p>
+            <p class="card-text">
+                <b>Genero: </b>${item.gender}
+            </p>
+            
         </div>
         </div>
         `
@@ -24,4 +33,4 @@ const apiRick = async (pagina) => {
     })
 } 
 
-apiRick(1);
+apiRick();
